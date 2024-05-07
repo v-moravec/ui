4a2213f723bc59e99b7624ef4608e345f6f46958
Vojtěch Moravec <voj.moravec@seznam.cz>
2024-05-07 15:10:44 +0200

    Update warning component.

--- apps/docs/components/content/Warning.vue
+++ apps/docs/components/content/Warning.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <UiCard class="not-prose flex items-center border-yellow-500 bg-yellow-100 p-3 text-yellow-500">
-    <Icon name="fa6-solid:exclamation" class="ml-1 mr-4 size-6" />
+  <UiCard class="not-prose flex items-center border-border bg-secondary p-3 text-secondary-contrast">
+    <span
+      class="ml-1 mr-4 flex aspect-square items-center justify-center rounded-lg bg-primary p-2.5 text-primary-contrast"
+    >
+      <Icon name="fa6-solid:exclamation" class="size-6" />
+    </span>
     <ContentSlot />
   </UiCard>
 </template>
